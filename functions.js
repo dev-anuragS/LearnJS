d8c3f1e0bd3fd547563a1567e8f1a73878e4402d
Anurag <anurag.astikint@gmail.com>
2021-11-30 13:50:14 +0530

    LEARN JS

---
+++ functions.js
@@ -0,0 +1,17 @@
+console.log("FUNCTIONS");
+
+function greet(name){
+    console.log(name+" is the name");
+}
+
+function sum(a,b,c){
+    return a+b+c;
+}
+
+greet('amit');
+greet('tami');
+greet('itam');
+greet('mita');
+greet('who');
+
+console.log(sum(1,2,3));
